1da356ed826a96a8bc628d2d5ef3d6dcaf9df6a7
Ilayda Beyreli <32909683+ibeyreli@users.noreply.github.com>
2020-05-30 13:52:44 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+"""
+test.py
+Forward pass test procedure of DeepND
+for replicating previous experiments and reproducing data
+Bilkent University, Department of Computer Engineering
+Ankara, 2020
+"""
