79b0371ab1df4b760347c2b829fca3c2a87d677f
Ilayda Beyreli <32909683+ibeyreli@users.noreply.github.com>
2020-06-01 17:37:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,57 @@ import pickle
 
 import os 
 os.environ["CUDA_VISIBLE_DEVICES"]= "0,1,2,3,4,5,6,7"
+if model_select:
+    featsizeid = 13 
+    featsizeasd = 18
+    featsize = 28 
+    h1 = 4
+    h2 = 2
+    lrasd = 0.0007
+    lrid = 0.007
+    lrc = 0.0007
+    wd = 0.0001
+    diseasename = "Multi"
+    model = DeepND()
+else:
+    if disease:
+        input_size = 13
+        l_rate = 0.0007  
+        diseasename = "ID"
+    else:
+        input_size = 18
+        l_rate = 0.0007 
+        diseasename = "ASD"
+    model = DeepND_ST(featsize=input_size)
+    
+if mode:
+    experiment = 11
+    if experiment < 10:
+        experiment = "0" + str(experiment)
+    print("Generating results for ", diseasename , " Exp :", experiment)
+    try:
+        os.mkdir(root + diseasename + "Exp" + str(experiment) + "Test", access_rights)
+    except OSError:
+        print ("Creation of the test directory failed")
+    else:
+        print ("Successfully created the test directory")
+    # Load random states for reproducing test results
+    torch.set_rng_state(torch.load(root + diseasename + "Exp" + str(experiment) + "/deepND_experiment_torch_random_state"))
+    state = np.random.get_state()
+    with open(root + diseasename + "Exp" + str(experiment) + "/deepND_experiment_numpy_random_state", 'rb') as f:
+        state = pickle.load(f)
+    np.random.set_state(state)
+else:
+    try:
+        os.mkdir(root + diseasename + "Exp" + str(experiment), access_rights)
+    except OSError:
+        print ("Creation of the directory for the results failed")
+    else:
+        print ("Successfully created the directory for the results")
+    torch.save(torch.get_rng_state(),root + diseasename + "Exp" + str(experiment) + "/deepND_experiment_torch_random_state")
+    state = np.random.get_state()
+    with open(root + diseasename + "Exp" + str(experiment) + "/deepND_experiment_numpy_random_state", 'wb') as f:
+        pickle.dump(state, f)
 
 ###############################################################################################################################################
 """LOADING NETWORKS"""
