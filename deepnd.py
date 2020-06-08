85f3382b14bc97d49d11990389de77d3b83f9c77
Ilayda Beyreli <32909683+ibeyreli@users.noreply.github.com>
2020-06-08 13:21:48 +0300

    Update deepnd.py

--- deepnd.py
+++ deepnd.py
@@ -5,6 +5,10 @@ for replicating previous experiments and reproducing data
 Bilkent University, Department of Computer Engineering
 Ankara, 2020
 """
+
+geneNames_all = pd.read_csv(root + "/Data/Brainspan/row-genes.txt", header = None)
+geneNames_all = geneNames_all[0].tolist()
+geneDict = constructGeneDictionary(root + "/Data/Brainspan/hugogenes_entrez.txt")
 ###############################################################################################################################################
 """GOLD STANDARDS"""
 ###############################################################################################################################################
